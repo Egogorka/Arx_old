73186422bfad0381ef89697dced3953aafc10452
Egogorka <bike034@mail.ru>
2021-12-18 01:06:17 +0300

    perlin noise test

--- utility/PerlinNoise.cpp
+++ utility/PerlinNoise.cpp
@@ -50,6 +50,54 @@ std::vector<float> PerlinNoise2D( const std::vector<float>& seed, int nOctaves )
     return std::move(output);
 }
 
-int testNoise() {
+#include <SFML/Graphics.hpp>
+#include <vector>
 
+int testNoise() {
+    std::vector<float> seed(256);
+    for (auto& elem : seed){
+        elem = (float)rand() / (float)RAND_MAX;
+    }
+
+    sf::RenderWindow window;
+    window.create(sf::VideoMode(512, 512), "Test", sf::Style::Default);
+    window.setFramerateLimit(60);
+
+    int n = 5;
+
+    while(true){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed) {
+                goto EXIT;
+            } else
+            if(event.type == sf::Event::KeyPressed) {
+                if(event.key.code == sf::Keyboard::A) {
+                    n += 1;
+                } else
+                if(event.key.code == sf::Keyboard::B) {
+                    n -= 1;
+                } else
+                if(event.key.code == sf::Keyboard::C) {
+                    for (auto& elem : seed){
+                        elem = (float)rand() / (float)RAND_MAX;
+                    }
+                }
+            }
+        }
+        window.clear(sf::Color::White);
+
+        auto noise = PerlinNoise1D(seed, n);
+        for (int i = 0; i < noise.size(); ++i) {
+            sf::RectangleShape rec;
+//            std::cout << noise[i] << '\n';
+            rec.setFillColor(sf::Color::Black);
+            rec.setSize(sf::Vector2f(2, 256*noise[i]));
+            rec.setPosition(sf::Vector2f(2*i,0));
+            window.draw(rec);
+        }
+        window.display();
+    }
+    EXIT:
+    return 0;
 }
